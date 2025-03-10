058a3f97d59175b09d42f7d13d67fbcc497c559c
Reynold254 <reynold.burudi@student.moringaschool.com>
2025-03-10 14:02:27 +0200

    Alive

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+// Declare a constant variable called companyName and set it to 'Scuber'
+const companyName = "Scuber";
+
+// Declare a variable called mostProfitableNeighborhood and set it to 'Chelsea'
+let mostProfitableNeighborhood = "Chelsea";
+
+// Declare a variable called companyCeo and set it to 'Susan Smith'
+let companyCeo = "Susan Smith";
 // Code your solution in this file!
